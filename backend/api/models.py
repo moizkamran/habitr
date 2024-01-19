01e69fff9ce9548e98a662d24096e51de38dfbb7
Abdulmoeez Kamran <70318057+moizkamran@users.noreply.github.com>
2024-01-20 04:14:51 +0500

    Progress Class

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Habit(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
-    frequency = models.IntegerField(max_length=7, default=1)
+    frequency = models.IntegerField(default=1)
     start_date = models.DateField()
     goal_date = models.DateField()
     completed = models.BooleanField(default=False)
-    streak = models.IntegerField(max_length=7, default=0, null=False)
+    streak = models.IntegerField(default=0, null=False)
+
+class Progress(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    dates = models.DateField()
