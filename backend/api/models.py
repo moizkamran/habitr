4127cfc87db63b2d4d4267bed6375fe3b6fff88d
Abdulmoeez Kamran <70318057+moizkamran@users.noreply.github.com>
2024-01-20 04:08:51 +0500

    Habit Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Habit(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    frequency = models.IntegerField(max_length=7, default=1)
+    start_date = models.DateField()
+    goal_date = models.DateField()
+    completed = models.BooleanField(default=False)
+    streak = models.IntegerField(max_length=7, default=0, null=False)
