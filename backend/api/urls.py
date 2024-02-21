f5a42dafd339b7b5df0070825a0f99baf1f16519
Abdulmoeez Kamran <70318057+moizkamran@users.noreply.github.com>
2024-02-21 05:53:56 +0500

    Create Operation

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HabitView
+from .views import HabitView, CreateHabitView
 
 urlpatterns = [
     path("habit", HabitView.as_view()),
+    path("create-habit", CreateHabitView.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Habit
-from .serializers import HabitSerializer
+from .serializers import HabitSerializer, CreateHabitSerializer
+from rest_framework.views import APIView
 
 # Create your views here.
 
 class HabitView(generics.ListCreateAPIView):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+
+class CreateHabitView(APIView):
+    serializer_class = CreateHabitSerializer
+
+    def post(self, request, format=None):
+        serializer = CreateHabitSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
