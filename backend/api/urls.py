e7d2ba4e5298991f6ae269fab610b68574e31f6f
Abdulmoeez Kamran <70318057+moizkamran@users.noreply.github.com>
2024-02-21 05:02:56 +0500

    CURD

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HabitView
+from .views import HabitView, HabitDetailView
 
 urlpatterns = [
-    path("habit", HabitView.as_view()),
+    path("habit/", HabitView.as_view()),
+    path("habit/<int:pk>/", HabitDetailView.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Habit
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import HabitSerializer
 
-# Create your views here.
-
-class HabitView(generics.ListCreateAPIView):
+class HabitView(generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+
+class HabitDetailView(generics.RetrieveDestroyAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"detail": "Habit has been deleted"}, status=status.HTTP_204_NO_CONTENT)
