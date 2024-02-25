8eac49aafe5d15cbafda7f1ea92983e2e056cd09
Abdulmoeez Kamran <70318057+moizkamran@users.noreply.github.com>
2024-02-25 05:33:52 +0500

    Complete Habit

--- frontend/src/HomeAssets/TodaysHabits.jsx
+++ frontend/src/HomeAssets/TodaysHabits.jsx
@@ -1,11 +1,22 @@
 import { ActionIcon, Flex, Text } from '@mantine/core'
-import { IconCheck, IconChessBishop, IconCircle, IconCircleFilled, IconTrash, IconYoga } from '@tabler/icons-react'
+import { IconCheck, IconChessBishop, IconChevronCompactLeft, IconChevronLeft, IconCircle, IconCircleFilled, IconTrash, IconYoga } from '@tabler/icons-react'
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 
 const bg_colors = ["#ef476f", "#ffd166", "#06d6a0", "#118ab2", "#118ab2"]
 
 const TodaysHabits = ({ todaysDate, habits, getHabits }) => {
+    console.log(todaysDate)
+
+    const filteredHabits = habits.filter(habit => {
+        const start_date = new Date(habit.start_date)
+        const goal_date = new Date(habit.goal_date)
+        const today = todaysDate
+
+        return start_date <= today && goal_date >= today
+    }
+    )
+
   return (
     <Flex direction={'column'} align={'center'} justify={'center'} gap={20}>
         <Flex justify={'space-between'} w={'100%'}>
@@ -16,9 +27,10 @@ const TodaysHabits = ({ todaysDate, habits, getHabits }) => {
             see all
             </Text>
         </Flex>
-        {habits.map((habit, index) => {
+        {filteredHabits.map((habit, index) => {
             return (
-                <SingleTask key={index} habit={habit} getHabits={getHabits} />
+                <SingleTask key={index} todaysDate={todaysDate}
+                habit={habit} getHabits={getHabits} />
             )
         })}
     </Flex>
@@ -27,12 +39,40 @@ const TodaysHabits = ({ todaysDate, habits, getHabits }) => {
 
 export default TodaysHabits
 
-const SingleTask = ({ habit, getHabits }) => {
+const SingleTask = ({ habit, getHabits, todaysDate }) => {
 
     const random_bg = Math.floor(Math.random() * bg_colors.length)
+    const completions = habit.completions
+    const [showHidden, setShowHidden] = useState(false)
+
+    //from completions array, check if it has a completion for today
+    //if yes, display a checkmark
+    //if no, display a circle
+    const todaysCompletion = completions.find(completion => {
+        const completion_date = new Date(completion.completion_date)
+        return completion_date.toDateString() === todaysDate.toDateString()
+    }
+    )
+    const timezoneOffsetMs = todaysDate.getTimezoneOffset() * 60 * 1000; // Get timezone offset in milliseconds
+    const localDate = new Date(todaysDate.getTime() - timezoneOffsetMs); // Adjust date to local timezone
+
+    const formattedDate = localDate.toISOString().split('T')[0]; // Format to YYYY-MM-DD
+
+
+    const handleCompletion = async () => {
+        try {
+            const response = await axios.post(`http://localhost:8000/api/habit-completion`, {
+                habit: habit.id,
+                completion_date: formattedDate,
+            })
+            getHabits()
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     const goal_date = new Date(habit.goal_date)
-    const goal_days_left = Math.floor((goal_date - new Date()) / (1000 * 60 * 60 * 24))
+    const goal_days_left = Math.floor((goal_date - todaysDate) / (1000 * 60 * 60 * 24))
     const handleDeleteHabit = async () => {
         try {
             const response = await axios.delete(`http://localhost:8000/api/delete-habit/${habit.id}`)
@@ -84,7 +124,8 @@ const SingleTask = ({ habit, getHabits }) => {
             </Flex>
         </Flex>
         <Flex align={'center'} gap={10}>
-            <ActionIcon
+            <IconChevronLeft size={20} color={'black'} onClick={() => setShowHidden(!showHidden)}/>
+            {showHidden && (<ActionIcon
             size={'xl'}
             variant={'light'}
             color={'red'}
@@ -92,11 +133,12 @@ const SingleTask = ({ habit, getHabits }) => {
             onClick={handleDeleteHabit}
             >
                 <IconTrash size={20} />
-            </ActionIcon>
+            </ActionIcon>)}
             <ActionIcon
             size={'xl'}
             variant={'light'}
-            color={'grey'}
+            onClick={handleCompletion}
+            color={todaysCompletion ? 'green' : 'grey'}
             radius={'xl'}
             >
                 <IconCheck size={20} />
